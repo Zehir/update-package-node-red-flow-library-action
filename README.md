caaf3647173de0af44e1ac246b6ca9e2f4a54539
Zehir <zehir@zorim.fr>
2021-10-14 00:47:03 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# update-package-node-red-flow-library-action
+# Update Node-Red flow-library
+
 Update or add a Node-Red package on the Node-Red flow library (flows.nodered.org).
+
+## Inputs
+
+## `package-name`
+
+**Required** The name of the package. Default `""`.
+
+## Outputs
+
+## `result`
+
+The result of update
+
+## Example usage
+
+```
+uses: actions/update-package-node-red-flow-library-action@v1.0.0
+with:
+  package-name: 'node-red-contrib-deconz'
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const got = require('got');
+const {parse} = require("node-html-parser");
+const {CookieJar} = require('tough-cookie');
+
+const flowLibraryUrl = 'https://flows.nodered.org/add/node';
+
+(async () => {
+    try {
+        const packageName = core.getInput('package-name');
+        if (packageName === undefined || packageName === '') {
+            core.setFailed("No package name provided.");
+        }
+        try {
+            const cookieJar = new CookieJar();
+            const response = await got(flowLibraryUrl, {cookieJar});
+            const root = parse(response.body);
+            const csrf = root.querySelector('#add-node-csrf').getAttribute('value');
+            if (csrf !== undefined) {
+                const response2 = await got.post(flowLibraryUrl, {
+                    cookieJar,
+                    form: {
+                        module: packageName,
+                        _csrf: csrf
+                    }
+                });
+                core.setOutput('result', response2.body);
+            }
+        } catch (error) {
+            core.setOutput('result', error.response.body);
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
